b5850d27cd54195dae470c8a5dd732c4026183a0
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-02-22 19:40:25 +0200

    Some message

--- first_exeric.py
+++ first_exeric.py
@@ -1,1 +1,17 @@
-print('Hello')
+import random
+choice = input(str('tasteaza "da" daca vrei sa incupi jocul sau "nu" daca nu vrei: '))
+if choice == "nu":
+   print('hai pa')
+elif choice == "da":
+   num = input("ghiceste-mi numarul dintre 1 si 100: ")
+   numar = random.randint(1, 100)
+   for x in numar:
+      if x < num:
+         input("numarul meu este mai mic")
+      elif x > num:
+         input("mai meu este mai mare")
+      else:
+         print("da bravo")
+else:
+   print(" nu e voe ")
+   print(" nu e voe ")
