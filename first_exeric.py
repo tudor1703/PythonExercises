90bbc00661fb148700e213439734603fae309f68
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-02-23 22:54:33 +0200

    accessing_a_file

--- first_exeric.py
+++ first_exeric.py
@@ -1,20 +1,4 @@
-import random
-choice = input(str('tasteaza "da" daca vrei sa incupi jocul sau "nu" daca nu vrei: '))
-if choice == "nu":
-   print('hai pa')
-elif choice == "da":
-   num = input("ghiceste-mi numarul dintre 1 si 100: ")
-   numar = random.randint(1, 100)
-   while x = numar:
-      if x < num:
-         input("numarul meu este mai mic")
-      elif x > num:
-         input("mai meu este mai mare")
-      else:
-         print("da bravo")
-else:
-   print(" nu e voe ")
-   print(" nu e voe ")
-   print(" nu e voe ")
-   print(" nu e voe ")
-   print('bravooo')
+f = open("python.txt", "r")
+t = f.read()
+print(t)
+f.clse()
