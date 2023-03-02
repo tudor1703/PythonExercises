75ab97769f437db79c8baf9177db32c401266071
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-03-02 21:33:14 +0200

    renaming_the_tab

---
+++ main.py
@@ -0,0 +1,13 @@
+given_file = open('input.txt', 'r')
+
+lines = given_file.readlines()
+sum = 0
+
+for line in lines:
+    for c in line:
+        if c.isdigit() == True:
+            sum = sum + int(c)
+
+print(sum)
+
+given_file.close()
