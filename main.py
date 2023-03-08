fb5aa9dc8baf730eb1138a91cb47e7b956ba0cda
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-03-08 13:19:37 +0200

    output_the_sum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 with open("input.txt", "r") as f:
     score = f.read()
     score_ints = [ int(x) for x in score.split() ] 
-    print(sum(score_ints))g
+    the_sum = print(sum(score_ints))
+    with open("output.txt", "w") as infile:
+        infile.write(str(the_sum))
