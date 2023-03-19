4961024bb19c8f649b75ab049b25f1d565b19e22
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-03-19 18:20:33 +0200

    message_development

--- phone_list.py
+++ phone_list.py
@@ -1,14 +1,14 @@
 import sys
 
-arg = sys.argv[1]
+insert = sys.argv[1]
 
-if arg == '1':
+if insert == '1':
     fname = input('enter the first name: ')
     lname = input('enter the last name: ')
     phone_num = input('enter the phone number: ')
     new_phone = fname, lname, phone_num
 else:
-    print('error')
+    print('the command entered does not exist')
 
 def new_func(new_phone):
     with open('phone.txt', 'w') as f:
