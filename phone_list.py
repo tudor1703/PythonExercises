19dfe177094f165277fadbd6c67b1e6b54f7a1c1
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-03-19 10:42:58 +0200

    new_choice

--- phone_list.py
+++ phone_list.py
@@ -1,8 +1,18 @@
-fname = input('enter the first name: ')
-lname = input('enter the last name: ')
-phone_num = input('enter the phone number:')
-new_phone = fname, lname, phone_num 
+import sys
 
-with open('phone.txt', 'w') as f:
-    f.write(str(new_phone))
-print('A new object has been successfully saved!')
+arg = sys.argv[1]
+
+if arg == '1':
+    fname = input('enter the first name: ')
+    lname = input('enter the last name: ')
+    phone_num = input('enter the phone number: ')
+    new_phone = fname, lname, phone_num
+else:
+    print('error')
+
+def new_func(new_phone):
+    with open('phone.txt', 'w') as f:
+        f.write(str(new_phone))
+    print('A new object has been successfully saved!')
+
+new_func(new_phone)
