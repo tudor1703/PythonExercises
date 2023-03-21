5d769c098945c6a1468210f5546b1461f6269634
tudor1703 <118926933+tudor1703@users.noreply.github.com>
2023-03-21 18:07:20 +0200

    I_can_see_resulsts

--- phone_list.py
+++ phone_list.py
@@ -2,17 +2,23 @@ import sys
 
 argv = sys.argv[1]
 
-if argv == 'insert':
-    fname = input('enter the first name: ')
-    lname = input('enter the last name: ')
-    phone_num = input('enter the phone number: ')
-    new_phone = fname, lname, phone_num
-else:
-    print('the command entered does not exist')
+def list_func():
+    with open('phone.txt', 'r') as t:
+        for line in t:
+            print(line.rstrip())
 
 def new_func(new_phone):
     with open('phone.txt', 'w') as f:
         f.write(str(new_phone))
     print('A new object has been successfully saved!')
 
-new_func(new_phone)
+if argv == 'insert':
+    fname = input('enter the first name: ')
+    lname = input('enter the last name: ')
+    phone_num = input('enter the phone number: ')
+    new_phone = fname, lname, phone_num
+    new_func(new_phone)
+
+elif argv == 'list':
+    list_func()
+
